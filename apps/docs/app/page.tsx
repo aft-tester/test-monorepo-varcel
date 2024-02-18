de4a8b93081e50d70197ec683c31ea01523afcbf
aft-tester <test.aft@outlook.com>
2024-02-18 08:34:56 +0300

    changed main page to anync

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -51,7 +51,7 @@ const LINKS = [
   },
 ];
 
-export default function Page(): JSX.Element {
+export default async function Page() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
